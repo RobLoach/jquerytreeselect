6950f5e170d6a94c2fe92c2ab825eda073052547
Rob Loach <robloach@gmail.com>
2014-06-02 18:33:38 -0400

    Initial commit

---
+++ jquerytreeselect.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+/**
+ * Provide the behaviors to register the jQuery Tree Select widget.
+ */
+Drupal.behaviors.jquerytreeselect = {
+  attach: function (context) {
+  }
+};
+
+})(jQuery);
